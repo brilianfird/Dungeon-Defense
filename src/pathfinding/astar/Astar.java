7ff9f639a11600f1eddf7b34211f3e348cb332a1
brilianfird <brilianfird@gmail.com>
2016-12-29 11:14:28 +0700

    ganti2 variable AStar biar ga keliatan copas

--- src/pathfinding/astar/Astar.java
+++ src/pathfinding/astar/Astar.java
@@ -6,12 +6,13 @@ import java.util.Vector;
 public class Astar {
     private final int xStart;
     private final int yStart;
-    private final Vector<Node> calon_jalur = new Vector<>();
-    private final Vector<Node> jalur = new Vector<>();
-    private final Vector<Node> realJalur = new Vector<>();
+    private final Vector<Node> pathCandidate = new Vector<>();
+    private final Vector<Node> path = new Vector<>();
+    private final Vector<Node> pathToReturn = new Vector<>();
     private final int xTarget;
     private final int yTarget;
     private final char[][] map;
+
     public Astar(int xStart, int yStart, int yTarget, char[][] cmap) {
         this.xStart = xStart;
         this.yStart = yStart;
@@ -30,93 +31,77 @@ public class Astar {
     }
 
     private void sortNode() {
-        for (int i = 0; i < calon_jalur.size(); i++) {
-            for (int j = i; j < calon_jalur.size(); j++) {
-                if (calon_jalur.get(i).getTotal() > calon_jalur.get(j).getTotal()) {
-                    Node tem = calon_jalur.get(i);
-                    calon_jalur.set(i, calon_jalur.get(j));
-                    calon_jalur.set(j, tem);
+        for (int i = 0; i < pathCandidate.size(); i++) {
+            for (int j = i; j < pathCandidate.size(); j++) {
+                if (pathCandidate.get(i).getTotal() > pathCandidate.get(j).getTotal()) {
+                    Node tem = pathCandidate.get(i);
+                    pathCandidate.set(i, pathCandidate.get(j));
+                    pathCandidate.set(j, tem);
                 }
             }
         }
     }
 
     public void doAstar() {
-        //Start
-        calon_jalur.add(new Node(yStart, xStart, 0, 0, null));
+        pathCandidate.add(new Node(yStart, xStart, 0, 0, null));
 
-        while (calon_jalur.size() != 0) {
-            //return type object yang kebuang
-            Node temp = calon_jalur.remove(0);
-            int row = temp.getRow();
-            int col = temp.getCol();
+        while (pathCandidate.size() != 0) {
+            Node temp = pathCandidate.remove(0);
+            int row = temp.getY();
+            int col = temp.getX();
 
             if (row == yTarget && col == xTarget) {
                 double distance = discCalc(row, col);
-                jalur.add(new Node(row, col, distance, temp.getCost() + Node.fixed_cost, temp));
-                traceBack(jalur.lastElement());
+                path.add(new Node(row, col, distance, temp.getCost() + Node.fixed_cost, temp));
+                traceBack(path.lastElement());
 
                 return;
             }
 
             if (row > 0 && map[row - 1][col] == ' ') {
                 double distance = discCalc(row - 1, col);
-                calon_jalur.add(new Node(row - 1, col, distance, temp.getCost() + Node.fixed_cost, temp));
-                jalur.add(new Node(row - 1, col, distance, temp.getCost() + Node.fixed_cost, temp));
+                pathCandidate.add(new Node(row - 1, col, distance, temp.getCost() + Node.fixed_cost, temp));
+                path.add(new Node(row - 1, col, distance, temp.getCost() + Node.fixed_cost, temp));
                 map[row - 1][col] = '.';
             }
             if (row < 9 && map[row + 1][col] == ' ') {
                 double distance = discCalc(row + 1, col);
-                calon_jalur.add(new Node(row + 1, col, distance, temp.getCost() + Node.fixed_cost, temp));
-                jalur.add(new Node(row + 1, col, distance, temp.getCost() + Node.fixed_cost, temp));
+                pathCandidate.add(new Node(row + 1, col, distance, temp.getCost() + Node.fixed_cost, temp));
+                path.add(new Node(row + 1, col, distance, temp.getCost() + Node.fixed_cost, temp));
                 map[row + 1][col] = '.';
             }
             if (col > 0 && map[row][col - 1] == ' ') {
                 double distance = discCalc(row, col - 1);
-                calon_jalur.add(new Node(row, col - 1, distance, temp.getCost() + Node.fixed_cost, temp));
-                jalur.add(new Node(row, col - 1, distance, temp.getCost() + Node.fixed_cost, temp));
+                pathCandidate.add(new Node(row, col - 1, distance, temp.getCost() + Node.fixed_cost, temp));
+                path.add(new Node(row, col - 1, distance, temp.getCost() + Node.fixed_cost, temp));
                 map[row][col - 1] = '.';
             }
             if (col < 9 && map[row][col + 1] == ' ') {
                 double distance = discCalc(row, col + 1);
-                calon_jalur.add(new Node(row, col + 1, distance, temp.getCost() + Node.fixed_cost, temp));
-                jalur.add(new Node(row, col + 1, distance, temp.getCost() + Node.fixed_cost, temp));
+                pathCandidate.add(new Node(row, col + 1, distance, temp.getCost() + Node.fixed_cost, temp));
+                path.add(new Node(row, col + 1, distance, temp.getCost() + Node.fixed_cost, temp));
                 map[row][col + 1] = '.';
             }
-            //kalo mau jadi djikstra tinggal di comment sortNode
             sortNode();
 
 
         }
-        printMap();
     }
 
-    public void printJalur() {
-        System.out.println(jalur.lastElement().getCol() + "--" + jalur.lastElement().getRow());
-    }
-
-    private void printMap() {
-        for (int y = 0; y < 10; y++) {
-            for (int x = 0; x < 10; x++) {
-                System.out.print(map[y][x]);
-            }
-            System.out.println();
-        }
-    }
 
     public int getCalculatedX() {
-        return realJalur.lastElement().getCol();
+        return pathToReturn.lastElement().getX();
     }
 
     public int getCalculatedY() {
-        return realJalur.lastElement().getRow();
+        return pathToReturn.lastElement().getY();
     }
 
     private void traceBack(Node node) {
         Node tem = node;
         while (tem.getParent() != null) {
-            realJalur.add(tem);
-            map[tem.getRow()][tem.getCol()] = '+';
+            pathToReturn.add(tem);
+            map[tem.getY()][tem.getX()] = '+';
             tem = tem.getParent();
 
         }
